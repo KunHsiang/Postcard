a5d94a73c82b26583f9cfc52f433d12adcff5d91
ChangKH <web@kunhsiang.com>
2014-12-02 21:42:14 +0800

    add UI and Code

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.hidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,13 +25,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet weak var sss: UITextField!
-
-    @IBOutlet weak var aaa: UITextField!
-    @IBAction func qqq(sender: AnyObject) {
-        //Adding a Comment here to test commits
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
         
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent",forState: UIControlState.Normal)
     }
 }
-
